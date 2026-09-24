cdbc5a6c82e325209f1faa83919858d74246f382
Parker.Dev <Doglover5749yt@gmail.com>
2026-09-24 13:53:13 -0500

    New Files
    added backend and doccumentation in the back

---
+++ Software/FrontEnd/BPL_HMI.py
@@ -0,0 +1,13 @@
+# GCV GN2 Check Valve
+# GPR GN2 Pressure Regulator
+# GBS GN2 Back Pressurizing Solenoid
+# GPRV GN2 Pressure Relief Valve
+# FPS Fuel Purging Solenoid
+# FMV Fuel Main Valve
+# FCV Fuel Check Valve
+# OMV Oxidizer Main Valve
+# OCV Oxidizer Check Valve
+# CCV Chiller Check Valve
+# ORIPS Oxidizer Run Tank Purge Solenoid
+# CPS Chiller Purge Solenoid
+# CMS Chiller Main Solenoid
